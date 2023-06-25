b53fc9a807ec534f93877c18d7615b9623f9a779
Felipe <felipemouradev@gmail.com>
2023-06-25 13:45:03 -0500

    just come more stuff

--- first-game/helpers.lua
+++
@@ -1,9 +0,0 @@
-
--- Set color for graphics
-function SetColor(r, g, b, a)
-  love.graphics.setColor(r, g, b, a)
-end
-
-function SetFont(font)
-  love.graphics.setFont(font)
-end
--- first-game/main.lua
+++ first-game/main.lua
@@ -1,5 +1,14 @@
+love = require('love')
 function love.load()
   num = 0
+  -- Set color for graphics
+  function SetColor(r, g, b, a)
+    love.graphics.setColor(r, g, b, a)
+  end
+
+  function SetFont(font)
+    love.graphics.setFont(font)
+  end
 end
 
 function love.update(dt)
@@ -7,5 +16,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.print(num, 400, 300)
+  love.graphics.print(num, 200, 200)
+  SetColor(1, 0, 0)
+  love.graphics.ellipse("fill", 400, 300, 200, 200)
 end
