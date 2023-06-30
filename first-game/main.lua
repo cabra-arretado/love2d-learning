fd65c196131bba8a6185ab78507c7977cfc9064a
Felipe <felipemouradev@gmail.com>
2023-06-30 16:29:02 -0500

    elipse moving

--- first-game/main.lua
+++ first-game/main.lua
@@ -1,23 +1,16 @@
 love = require('love')
 function love.load()
+  gameState = "on"
   frames_per_second = 0
+  ellipse = { x = 400, y = 300, width = 200, height = 200 }
   -- Set color for graphics
   function SetColor(r, g, b, a)
     love.graphics.setColor(r, g, b, a)
   end
 
-  function SetFont(font)
-    love.graphics.setFont(font)
-  end
-end
-
-function love.update(dt)
-  frames_per_second = frames_per_second + 1
-end
-
-function love.draw()
-  function move_object(object)
-    -- use arrow in keyboard to move object 1 pixel for each press 
+  function move_object(object, velocity)
+    -- use arrow in keyboard to move object 1 pixel for each press
+    velocity = velocity or 1
     if love.keyboard.isDown("right") then
       object.x = object.x + 1
     elseif love.keyboard.isDown("left") then
@@ -28,8 +21,18 @@ function love.draw()
       object.y = object.y + 1
     end
   end
-  love.graphics.print("Frames: " .. frames_per_second, 0, 0)
-  ellipse = love.graphics.ellipse("fill", 400, 300, 200, 200)
-  move_object(ellipse)
 
+  function SetFont(font)
+    love.graphics.setFont(font)
+  end
+end
+
+function love.update(dt)
+  frames_per_second = love.timer.getFPS()
+  move_object(ellipse, 2)
+end
+
+function love.draw()
+  love.graphics.print("Frames: " .. frames_per_second, 0, 0)
+  love.graphics.ellipse("line", ellipse.x, ellipse.y, ellipse.width, ellipse.height)
 end
